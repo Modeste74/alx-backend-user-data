7664d8ef83bc840964bfcb2963c22355f39e59fb
Modeste Ciira <ciiramodeste23@gmail.com>
2024-01-10 21:41:07 +0300

    Regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""defines classes and functions on user-data protection
+in logging datas"""
+import re
+from typing import List
+
+
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str) -> str:
+    """returns the log message obfuscated"""
+    obf = re.compile(
+        fr'({separator}|^)({"|".join(fields)})=([^{separator}]+)')
+    return obf.sub(fr'\1\2={redaction}{separator}', message)
