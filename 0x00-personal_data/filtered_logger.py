e0aabbeab7acfcf3b610d53f324859e7a5bb91fc
Modeste Ciira <ciiramodeste23@gmail.com>
2024-01-11 11:07:35 +0300

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,22 @@
 in logging datas"""
 import re
 from typing import List
+import logging
+
+
+'''class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError'''
 
 
 def filter_datum(
@@ -11,5 +27,5 @@ def filter_datum(
     """returns the log message obfuscated"""
     return re.sub(
         fr'({separator}|^)({"|".join(fields)})=([^{separator}]+)',
-        fr'\1\2={redaction}{separator}', message
+        fr'\1\2={redaction}', message
     )
