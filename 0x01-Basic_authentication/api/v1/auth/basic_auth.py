702bb98b50d9c02117268896817057661bc02697
Modeste74 <ciiramodeste23@gmail.com>
2024-01-16 00:36:33 -1200

    Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """defines a sub class BasicAuth"""
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -46,3 +47,18 @@ class BasicAuth(Auth):
         user_email, user_password = decoded_base64_authorization_header.split(
                 ':', 1)
         return user_email, user_password
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> User:
+        """ returns the User instance based on his email and password"""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        user_list = User.search({"email": user_email})
+        if not user_list:
+            return None
+        found_user = user_list[0]
+        if not found_user.is_valid_password(user_pwd):
+            return None
+        return found_user
