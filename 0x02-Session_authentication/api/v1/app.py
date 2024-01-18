cabeef89d36332619e1786c9c0858d6c5c0a1eba
Modeste74 <ciiramodeste23@gmail.com>
2024-01-18 01:33:26 -1200

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -57,11 +57,13 @@ def before_request_handler():
     excluded_paths = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            '/api/v1/forbidden/']
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
     auth_header = auth.authorization_header(request)
-    if auth_header is None:
+    session_cookie = auth.session_cookie(request)
+    if auth_header is None and session_cookie is None:
         abort(401)
     current_user = auth.current_user(request)
     if current_user is None:
