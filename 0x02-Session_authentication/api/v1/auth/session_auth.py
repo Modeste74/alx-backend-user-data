35d432c2eb197904f1176d28ae00e8cc5b0d7ccd
Modeste74 <ciiramodeste23@gmail.com>
2024-01-17 23:46:03 -1200

    Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""defines a sub class SessionAuth that inherits
+from the super Auth"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """defines method for session authentication
+    implementation"""
+    pass
