0f4ea99d57805c331f30d333585d43c893f87158
Modeste74 <ciiramodeste23@gmail.com>
2024-01-18 22:38:46 -1200

    auth import error fix

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -6,14 +6,13 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-auth_type = os.environ.get("AUTH_TYPE")
+auth_type = getenv("AUTH_TYPE")
 if auth_type:
     if auth_type == 'auth':
         from api.v1.auth.auth import Auth
