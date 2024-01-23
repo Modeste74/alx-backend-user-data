6cb1a94683664056ba9d53d64ee8081cebe5edec
Modeste74 <ciiramodeste23@gmail.com>
2024-01-22 21:56:35 -1200

    create user and find user

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Adds new user to the databse
+        parameters:
+        - email: users email
+        - hashed_password: password that corresponds with user's
+        email.
+
+        Returns:
+        - User object representing the added user"""
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Finds user based on given criteria"""
+        try:
+            users = self._session.query(User).filter_by(**kwargs).all()
+            if not users:
+                raise NoResultFound
+            if len(users) == 1:
+                return users[0]
+            return users
+        except InvalidRequestError as e:
+            raise e
