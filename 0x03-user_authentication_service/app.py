9a6d908e1f34a6a59d9d19a64ad8bfdf60675411
Modeste74 <ciiramodeste23@gmail.com>
2024-01-24 07:13:07 -1200

    log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """sets up a basic flask app"""
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 app = Flask(__name__)
